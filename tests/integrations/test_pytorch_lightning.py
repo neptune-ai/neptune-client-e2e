4b752fd99380bb728c9480f4e61dce020bb2efff
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-09-11 03:20:05 +0200

    Test PTL NeptuneLogger

--- tests/integrations/test_pytorch_lightning.py
+++ tests/integrations/test_pytorch_lightning.py
@@ -14,77 +14,242 @@
 # limitations under the License.
 #
 import os
+import re
+import unittest
 
-import neptune_pytorch_lightning
-import pytorch_lightning as pl
+import neptune.new as neptune
+import numpy as np
 import torch
-from neptune.new.integrations.pytorch_lightning import NeptuneLogger
-from torch.nn import functional as F
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from sklearn.metrics import accuracy_score
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from torchvision.datasets import MNIST
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import NeptuneLogger
 
 from tests.integrations.common import does_series_converge
 
 PARAMS = {
     'max_epochs': 3,
+    'save_top_k': 2,
     'learning_rate': 0.005,
-    'batch_size': 32,
+    'decay_factor': 0.99,
+    'batch_size': 64,
+    'linear': 64
 }
 
 
 class LitModel(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, linear, learning_rate, decay_factor, neptune_logger):
         super().__init__()
-        self.lin1 = torch.nn.Linear(28 * 28, 10)
+        self.linear = linear
+        self.learning_rate = learning_rate
+        self.decay_factor = decay_factor
+        self.train_img_max = 10
+        self.train_img = 0
+        self.layer_1 = torch.nn.Linear(28 * 28, linear)
+        self.layer_2 = torch.nn.Linear(linear, 20)
+        self.layer_3 = torch.nn.Linear(20, 10)
+        self.neptune_logger = neptune_logger
 
     def forward(self, x):
-        # pylint: disable=arguments-differ
-        return torch.relu(self.lin1(x.view(x.size(0), -1)))  # pylint: disable=no-member
+        x = x.view(x.size(0), -1)
+        x = self.layer_1(x)
+        x = F.relu(x)
+        x = self.layer_2(x)
+        x = F.relu(x)
+        x = self.layer_3(x)
+        return x
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        scheduler = LambdaLR(optimizer, lambda epoch: self.decay_factor ** epoch)
+        return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
-        # pylint: disable=arguments-differ
         x, y = batch
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
-        self.log('train_loss', loss)
-        return loss
+        self.log("train/loss", loss, prog_bar=False)
+        y_true = y.cpu().detach().numpy()
+        y_pred = y_hat.argmax(axis=1).cpu().detach().numpy()
+        return {"loss": loss,
+                "y_true": y_true,
+                "y_pred": y_pred}
 
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=PARAMS['learning_rate'])
+    def training_epoch_end(self, outputs):
+        y_true = np.array([])
+        y_pred = np.array([])
+        for results_dict in outputs:
+            y_true = np.append(y_true, results_dict["y_true"])
+            y_pred = np.append(y_pred, results_dict["y_pred"])
+        acc = accuracy_score(y_true, y_pred)
+        self.log(f"train/loader_acc", acc)
+
+    def validation_step(self, batch, batch_idx, dataset_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log("val/loss", loss, prog_bar=False)
+        y_true = y.cpu().detach().numpy()
+        y_pred = y_hat.argmax(axis=1).cpu().detach().numpy()
+        return {"loss": loss,
+                "y_true": y_true,
+                "y_pred": y_pred}
+
+    def validation_epoch_end(self, outputs):
+        for dl_idx in range(2):
+            y_true = np.array([])
+            y_pred = np.array([])
+            for results_dict in outputs[dl_idx]:
+                y_true = np.append(y_true, results_dict["y_true"])
+                y_pred = np.append(y_pred, results_dict["y_pred"])
+            acc = accuracy_score(y_true, y_pred)
+            self.log(f"val/loader_acc", acc)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log("test/loss", loss, prog_bar=False)
+        y_true = y.cpu().detach().numpy()
+        y_pred = y_hat.argmax(axis=1).cpu().detach().numpy()
+        for j in np.where(np.not_equal(y_true, y_pred))[0]:
+            img = np.squeeze(x[j].cpu().detach().numpy())
+            img[img < 0] = 0
+            img = img / np.amax(img)
+            self.neptune_logger.experiment["model_code/test/misclassified_images"].log(
+                neptune.types.File.as_image(img),
+                description="y_pred={}, y_true={}".format(y_pred[j], y_true[j]),
+            )
+        return {"loss": loss,
+                "y_true": y_true,
+                "y_pred": y_pred}
+
+    def test_epoch_end(self, outputs):
+        y_true = np.array([])
+        y_pred = np.array([])
+        for results_dict in outputs:
+            y_true = np.append(y_true, results_dict["y_true"])
+            y_pred = np.append(y_pred, results_dict["y_pred"])
+        acc = accuracy_score(y_true, y_pred)
+        self.log(f"test/acc", acc)
 
 
-class TestPytorchLightning:
-    def test_learn(self):
-        # given (Subject)
-        neptune_logger = NeptuneLogger(
-            close_after_fit=False,
+class MNISTDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size, normalization_vector):
+        super().__init__()
+        self.batch_size = batch_size
+        self.normalization_vector = normalization_vector
+        self.mnist_train = None
+        # self.mnist_val = None
+        self.mnist_val1 = None
+        self.mnist_val2 = None
+        self.mnist_test = None
+
+    def prepare_data(self):
+        MNIST(os.getcwd(), train=True, download=True)
+        MNIST(os.getcwd(), train=False, download=True)
+
+    def setup(self, stage):
+        # transforms
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize(self.normalization_vector[0],
+                                                             self.normalization_vector[1])])
+        if stage == "fit":
+            mnist_train = MNIST(os.getcwd(), train=True, transform=transform)
+            self.mnist_train, self.mnist_val1, self.mnist_val2 = random_split(mnist_train, [50000, 5000, 5000])
+        if stage == "test":
+            self.mnist_test = MNIST(os.getcwd(), train=False, transform=transform)
+
+    def train_dataloader(self):
+        mnist_train = DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=4)
+        return mnist_train
+
+    def val_dataloader(self):
+        mnist_val1 = DataLoader(self.mnist_val1, batch_size=self.batch_size, num_workers=4)
+        mnist_val2 = DataLoader(self.mnist_val2, batch_size=self.batch_size, num_workers=4)
+        return [mnist_val1, mnist_val2]
+
+    def test_dataloader(self):
+        mnist_test = DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=1)
+        return mnist_test
+
+
+class TestPytorchLightning(unittest.TestCase):
+    common_neptune_run = None
+
+    @classmethod
+    def setUpClass(cls):
+        # given
+        cls.common_neptune_run = neptune.init(
             name='Integration pytorch-lightning',
+            tags=['jk-test'],
         )
-        trainer = pl.Trainer(max_epochs=PARAMS['max_epochs'],
-                             logger=neptune_logger)
-
         # and
-        model = LitModel()
-        train_loader = DataLoader(MNIST(os.getcwd(), download=True, transform=transforms.ToTensor()),
-                                  batch_size=PARAMS['batch_size'])
-
-        # when
-        trainer.fit(model, train_loader)
-        run = neptune_logger.run
-        run.sync()
+        model_checkpoint = ModelCheckpoint(
+            dirpath="my_model/checkpoints/",
+            filename="{epoch:02d}-{val/loss/dataloader_idx_1:.2f}",
+            save_weights_only=True,
+            save_top_k=PARAMS['save_top_k'],
+            save_last=True,
+            monitor="val/loss/dataloader_idx_1",
+            every_n_epochs=1
+        )
+        neptune_logger = NeptuneLogger(
+            run=cls.common_neptune_run,
+            prefix='custom_prefix'
+        )
+        # and (Subject)
+        trainer = pl.Trainer(
+            max_epochs=PARAMS['max_epochs'],
+            log_every_n_steps=10,
+            logger=neptune_logger,
+            track_grad_norm=2,
+            callbacks=[model_checkpoint],
+        )
+        model = LitModel(
+            linear=PARAMS['linear'],
+            learning_rate=PARAMS['learning_rate'],
+            decay_factor=PARAMS['decay_factor'],
+            neptune_logger=neptune_logger
+        )
+        dm = MNISTDataModule(
+            normalization_vector=((0.1307,), (0.3081,)),
+            batch_size=PARAMS['batch_size']
+        )
 
         # then
+        trainer.fit(model, datamodule=dm)
+        trainer.test(model, datamodule=dm)
+        cls.common_neptune_run.sync()
+
+    def test_logging_values(self):
         # correct integration version is logged
-        logged_version = run['source_code/integrations/neptune-pytorch-lightning'].fetch()
-        assert logged_version == neptune_pytorch_lightning.__version__
+        logged_version = self.common_neptune_run['source_code/integrations/pytorch-lightning'].fetch()
+        assert logged_version == pl.__version__
 
         # epoch are logged in steps [1, 1, ...., 2, 2, ..., 3, 3 ...]
-        logged_epochs = list(run['metrics/epoch'].fetch_values()['value'])
+        logged_epochs = list(self.common_neptune_run['custom_prefix/epoch'].fetch_values()['value'])
         assert sorted(logged_epochs) == logged_epochs
         assert set(logged_epochs) == {0, 1, 2}
 
         # does train_loss converge?
-        training_loss = list(run['metrics/train_loss'].fetch_values()['value'])
-        # assert len(logged_epochs) == len(training_loss)
+        training_loss = list(self.common_neptune_run['custom_prefix/train/loss'].fetch_values()['value'])
         assert does_series_converge(training_loss)
+
+    def test_saving_models(self):
+        best_model_path = self.common_neptune_run['custom_prefix/model/best_model_path'].fetch()
+        assert re.match(r'.*my_model/checkpoints/epoch=.*-val/loss/dataloader_idx_1=.*\.ckpt$', best_model_path)
+        best_model_score = self.common_neptune_run['custom_prefix/model/best_model_score'].fetch()
+        assert 0 < best_model_score < 1
+
+        # make sure that exactly `save_top_k` checkpoints
+        # NOTE: when `max_epochs` is close to `save_top_k` there may be less than `save_top_k` saved models
+        checkpoints = self.common_neptune_run['custom_prefix/model/checkpoints'].fetch()
+        assert all((checkpoint.startswith('epoch=') for checkpoint in checkpoints))
+        assert len(checkpoints) == 2
