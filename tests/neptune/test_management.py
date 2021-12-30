69e464692ad577337a899d0be4c1ec26df1f8caf
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-12-30 16:52:58 +0100

    linter

--- tests/neptune/test_management.py
+++ tests/neptune/test_management.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from faker import Faker
 from datetime import datetime
+from faker import Faker
 import pytest
 
 from neptune.management import (
